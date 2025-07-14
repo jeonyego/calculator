28ac049735a6b0d7b9477768a183f3c673817c40
jeonyego <tkabio2024@gmail.com>
2025-07-14 18:56:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const fullSubjects = [
         '국어', '사회', '역사', '도덕', '수학', '과학', '기술가정',
-        '영어', '정보', '선택1', '선택2', '체육', '음악', '미술'
+        '영어', '선택1', '선택2', '선택3', '체육', '음악', '미술'
     ];
 
     const limitedSubjects = ['체육', '음악', '미술'];
